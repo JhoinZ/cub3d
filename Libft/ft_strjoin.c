e3e89e1defa5a65f62d15890de6af5f78d5a18a2
Filippo Saffirio <fsaffiri@c3r6s3.42malaga.com>
2025-10-28 12:55:55 +0100

    debugging

--- Libft/ft_strjoin.c
+++ Libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fsaffiri <fsaffiri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 14:25:35 by fsaffiri          #+#    #+#             */
-/*   Updated: 2025/10/25 15:51:51 by fsaffiri         ###   ########.fr       */
+/*   Updated: 2025/10/28 12:54:48 by fsaffiri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,33 +28,40 @@ char	*jointime(char *sjoin, const char *s2, size_t j)
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	j;
+	size_t 	len1;
+	size_t 	len2;
 	char	*sjoin;
-	size_t	i;
-	if (!s1 && !s2)
+	size_t	j;
+	if (s1 != NULL)
+		len1 = ft_strlen(s1);
+	else
+		len1 = 0;
+	if (s2 != NULL)
+		len2 = ft_strlen(s2);
+	else
+		len2 = 0;
+	if (len1 == 0 && len2 == 0)
 		return (NULL);
-	if (!s1)
-		return (ft_strdup(s2));
-	if (!s2)
-		return (ft_strdup(s1));
-	i = ft_strlen(s1) + ft_strlen(s2);
-	sjoin = (char *)malloc((i + 1) * sizeof(char));
+	sjoin = (char *)malloc((len1 + len2 + 1) * sizeof(char));
 	if (!sjoin)
 		return (NULL);
 	j = 0;
-	while (s1[j] != '\0')
+	if (s1 != NULL)
 	{
-		sjoin[j] = s1[j];
-		j++;
+		while (j < len1)
+		{
+			sjoin[j] = s1[j];
+			j++;
+		}
 	}
 	sjoin = jointime(sjoin, s2, j);
-	sjoin[i] = '\0';
+	sjoin[len1 + len2] = '\0';
 	return (sjoin);
 }
 
 char	*ft_strjoin_free(char *s1, char *s2, int free_flag)
 {
-	char *res;
+	char	*res;
 
 	res = ft_strjoin((const char *)s1, (const char *)s2);
 	if (free_flag & 1)
